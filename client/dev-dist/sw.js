e99a068a7af9a1aef56d6f2652ebd381f7c0154b
PasRP-Theo <theomertens12345@gmail.com>
2026-04-15 11:06:08 +0200

    tet

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fl3dej7aa14"
+    "revision": "0.qf4h8as174s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
