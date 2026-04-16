3e67159454885ea5107dff975e7597e76c9e2540
PasRP-Theo <theomertens12345@gmail.com>
2026-04-16 14:36:51 +0200

    ttest

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0h74rdo4eas"
+    "revision": "0.1ua6uhiuagk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
