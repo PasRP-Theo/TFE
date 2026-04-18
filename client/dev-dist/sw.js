2ddf3246ad2a46d39db6f0250c2f16b2b4c21046
PasRP-Theo <theomertens12345@gmail.com>
2026-04-18 15:42:23 +0200

    pi

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.irnlvt1ukc"
+    "revision": "0.7l6kckpj294"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
